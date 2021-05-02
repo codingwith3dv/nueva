28348e81667e2aa5fa1dd77cb1e48eacc52068e6
codingwith3dv <deveshvdddas@gmail.com>
2021-05-02 22:32:41 +0530

    dom updation is working

--- src/dom/DOMTree.ts
+++ src/dom/DOMTree.ts
@@ -6,14 +6,41 @@ export class DOMTree {
   }
   
   createElementVDOM(element: any) {
-    var tree = this;
+    let tree = this;
     if (typeof element === 'string') {
       return document.createTextNode(element);
     }
     let newElement = document.createElement(element.elementName)
     element.children
-      .map(child => tree.createElementVDOM(child))
+      .map((child: any) => tree.createElementVDOM(child))
       .forEach(newElement.appendChild.bind(newElement))
     return newElement;
+  }
+  
+  updateDOMElement(root: any, oldValue: any, newValue: any, indexForChild = 0) {
+    var tree = this;
+    if(oldValue == null){
+      root.appendChild(tree.createElementVDOM(newValue));
+    }else if(newValue == null) {
+      root.removeChild(root.childNodes[indexForChild]);
+    }else if(
+      typeof newValue !== typeof oldValue ||
+      typeof newValue === 'string' && 
+      newValue !== oldValue ||
+      newValue.elementName !== oldValue.elementName
+    ){
+      root.replaceChild(
+        tree.createElementVDOM(newValue), 
+        root.childNodes[indexForChild]
+      )
+    }else if(newValue.elementName) {
+      for (var i = 0; i < newValue.children.length || i < oldValue.children.length; i++) {
+        tree.updateDOMElement(
+          root.childNodes[indexForChild], 
+          oldValue.children[i], 
+          newValue.children[i]
+        )
+      }
+    }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,23 @@ export default class Main {
 }
 
 const a = DOMNode('ul', {},
-            DOMNode('li', {}, 'Item 1'),
-            DOMNode('li', {}, 'Item 2')
-          )
-          
+  DOMNode('li', {}, 'Item 1'),
+  DOMNode('li', {}, 'Item 2')
+)
+
+const b = DOMNode('ul', {},
+  DOMNode('button', {}, 'Item 2'),
+  DOMNode('li', {}, 'Item 3')
+)
+
 var root = document.getElementById('app');
 var tree = new DOMTree()
-root.appendChild(tree.createElementVDOM(a));
 
-console.log(a)
+var tr = document.getElementById('tr')
+
+if (root)
+  root.appendChild(tree.createElementVDOM(a));
+  
+tr.onclick = () => {
+  tree.updateDOMElement(root, a, b)
+}
