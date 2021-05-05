d4456519fcf852a318e9ace987894598f0d93ea1
codingwith3dv <deveshvdddas@gmail.com>
2021-05-05 14:37:26 +0530

    Rebuild

--- src/dom/DOMTree.ts
+++ src/dom/DOMTree.ts
@@ -19,29 +19,4 @@ export class DOMTree {
       .forEach(newElement.appendChild.bind(newElement))
     return newElement;
   }
-
-  updateDOMElement(root: any, oldTree: object, newTree: object, indexForChild = 0) {
-    var tree = this;
-
-  }
-
-  createDOMMap(root: Node | HTMLElement) {
-    var tree = this;
-    return Array.prototype.map.call(
-      root.childNodes,
-      (node: typeof root) => {
-
-        var details = {
-          content: node.childNodes && node.childNodes.length > 0 ? null : node.textContent,
-          atts: node.nodeType !== 1 ? [] : node.getAttribute(node.attributes),
-          type: node.nodeType === 3 ? 'text' : (node.nodeType === 8 ? 'comment' : node.tagName.toLowerCase()),
-          node: node
-        };
-        details.isSVG = details.type === 'svg';
-        details.children = tree.createDOMMap(node, details.isSVG);
-        return details;
-
-      }
-    )
-  }
 }
--- src/index.ts
+++ src/index.ts
@@ -5,22 +5,3 @@ export default class Main {
 
 }
 
-const newTree = DOMNode('ul', {}, 
-                  DOMNode('li', {}, 'Item 10'),
-                  DOMNode('li', {}, 'Item 20')
-                )
-                
-const domTree = DOMNode('ul', {}, 
-                  DOMNode('li', {}, 'Item 1'),
-                  DOMNode('li', {}, 'Item 2')
-                )
-                
-const root = document.querySelector('#app');
-var trigger = document.querySelector('#bt')
-
-var tree = new DOMTree();
-console.log(tree.createDOMMap(root));
-
-trigger.addEventListener('click', () => {
-  tree.updateDOMElement(root, domTree, newTree);
-});
