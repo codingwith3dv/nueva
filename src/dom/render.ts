cd4753ad0aa6abf70b958cb076a674b1efcb28d6
codingwith3dv <deveshvdddas@gmail.com>
2021-05-06 21:37:55 +0530

    quick fix

--- src/dom/render.ts
+++ src/dom/render.ts
@@ -2,11 +2,11 @@ import { VNode } from './VNode.js'
 
 export function render(node: VNode) {
 
-  let newNode: Node;
-  
-  if(typeof node._children[0] === 'string'){
-  newNode = document.createElement(node._elementName);
-  newNode.innerHTML = String(node._children);
-  return newNode;
-} 
+  let newNode: HTMLElement;
+
+  if (typeof node._children[0] === 'string') {
+    newNode = document.createElement(node._elementName) as HTMLElement;
+    newNode.innerHTML = String(node._children);
+    return newNode;
+  }
 }
