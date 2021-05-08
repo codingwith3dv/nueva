7ca7839f6664bf69155490ff8b08ebc949c50ee4
codingwith3dv <deveshvdddas@gmail.com>
2021-05-08 11:23:29 +0530

    hello1

--- src/dom/render.ts
+++ src/dom/render.ts
@@ -9,13 +9,5 @@ export function render(vnode: VNode, root: Node | HTMLElement) {
   let newNode = document.createElement(vnode._elementName) as HTMLElement;
   root.appendChild(newNode)
   
-  if(isString(vnode._children)) {
-    newNode.innerHTML = vnode._children as string;
-  }else if(isArray(vnode._children)) {
-    [vnode._children].forEach((item: VNode) => {
-      console.log(item._children);
-    })
-  }
-  
   return newNode;
 }
