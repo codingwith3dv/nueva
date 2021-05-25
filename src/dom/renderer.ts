797a5286ea9334011b74f29b023c3380522c9afc
codingwith3dv <deveshvdddas@gmail.com>
2021-05-25 20:46:13 +0530

    🙌 Feat: VTree now fully renders on window

--- src/dom/renderer.ts
+++ src/dom/renderer.ts
@@ -11,34 +11,36 @@ export const render = (
   container: Node
 ): Node => {
   const { type, textChild, children } = elemToRender;
-  if(!container) return null;
-  if(!type) {
+  if (!container) return null;
+  if (!type) {
     const newNode = document.createTextNode(textChild || '');
     container?.appendChild(newNode);
     return newNode;
   }
   const rootNode = document.createElement(type);
-  if(children) {
-    if(isArray(children)) {
-      renderChildren(children as Array<VElement>, rootNode);
+  if (children) {
+    if (isArray(children)) {
+      renderChildren(children as Array < VElement > , rootNode);
     }
   }
   container.appendChild(rootNode);
   return rootNode;
 }
 const renderChildren = (
-  children: Array<VElement> | string,
+  children: Array < VElement > | string,
   container: Node
 ) => {
-  isArray(children) && children.forEach((item: VElement) => {
-    if(item?.textChild) {
-     
-    } else if(item?.type) {
-      let newElem = document.createElement(item.type);
-      if(item?.textChild) newElem.appendChild(document.createTextNode(item.textChild));
-      else renderChildren(item?.children, newElem);
-      container.appendChild(newElem);
-    }
-  })
-  isString(children) && container.appendChild(document.createTextNode(children as string))
+  if (isArray(children)) {
+    children.forEach((item: VElement) => {
+      if (item?.textChild) {
+        container.appendChild(document.createTextNode(item.textChild as string));
+      } else if (item?.type) {
+        let newElem = document.createElement(item.type);
+        renderChildren(item.children ? item.children : item.textChild, newElem);
+        container.appendChild(newElem);
+      }
+    })
+  } else {
+    container.appendChild(document.createTextNode(children as string))
+  }
 }
